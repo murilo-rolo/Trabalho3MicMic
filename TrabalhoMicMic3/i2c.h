ca99f854a0c5fd985a3d587a86ca3c1ead912b41
Gilber <gilberljr@gmail.com>
2026-07-02 11:19:28 -0300

    (feat):Implementando I2C.c e I2C.h

--- TrabalhoMicMic3/i2c.h
+++ TrabalhoMicMic3/i2c.h
@@ -1,1 +1,14 @@
 #include <avr/io.h>
+#ifdef I2C_H
+#define I2C_H
+#include <stdint.h>
+
+void I2C_Init(void);
+
+void I2C_Start(void);
+
+void I2C_Stop(void);
+
+void I2C_Write(uint8_t data);
+
+#endif /* I2C_H */
