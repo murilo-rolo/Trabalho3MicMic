6b62713c5ec8f6ff579004b9954d6e982d6a717f
Murilo Rolo <murilorolo05@gmail.com>
2026-07-06 18:53:49 -0300

    adicionar Makefile do projeto

---
+++ TrabalhoMicMic3/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+eeprom.c
+
+i2c.c
+
+lcd.c
+
+main.c
+
+teclado.c
+
+timer.c
+
+pir.c
+
+uart.c
+
